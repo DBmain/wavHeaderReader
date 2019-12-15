689be1d273a1b02a9da79a22c53233df5b8bcfd3
Denis Boyarchuk <DBmain@ya.ru>
2019-12-15 22:45:14 +0300

    wtf

---
+++ wavHeaderReader/whr.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace wavHeadReader
+{
+    // channels = 22; bitDepth = 34; freq = 24-27; bps = 28-31; dataSize = 36-39
+    //
+    public class Wave
+    {
+        public byte channels;
+        public byte bitDepth;
+        public int freq;
+        public int bitsPerSample;
+        public int dataSize;
+        public int chunkSize;
+        private byte[] header = new byte[44];
+        public void recieveFile(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(header, 0, header.Length);
+                fs.Close();
+            }
+            workWithFile();
+        }
+        public void recieveFile(byte[] binary)
+        {
+            for (int i = 0; i < 44; i++)
+            {
+                header[i] = binary[i];
+            }
+            workWithFile();
+        }
+        private void workWithFile()
+        {
+            channels = header[22];
+            bitDepth = header[34];
+            freq = BitConverter.ToInt32(header, 24);
+            bitsPerSample = BitConverter.ToInt32(header, 28);
+            dataSize = BitConverter.ToInt32(header, 36);
+            chunkSize = BitConverter.ToInt32(header, 4);
+
+        }
+    }
+
+}
